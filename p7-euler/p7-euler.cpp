44747a600ae5a89cb442cee7a3ffa832686afb82
chaege_Thinkpad <c.haege@ipc.uni-stuttgart.de>
2019-03-13 00:35:48 +0100

    Added Visual Studio project

---
+++ p7-euler/p7-euler.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+
+
+int main(int argc, char **argv)
+{
+	unsigned long long p7_answ = 0;
+
+
+	std::cout << "The answer to the seventh problem of ProjectEuler.Net is " << p7_answ << ".\n";
+
+	return 0;
+}
